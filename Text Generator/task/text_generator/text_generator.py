933a1f23be6999281cb9a7b49a0e1ab6fc8eb791
IgnatIvanov <ignativanov1996@mail.ru>
2021-03-22 20:29:47 +0300

    2021.03.22.2029
    Stage 6 complete. Project complete.

--- Text Generator/task/text_generator/text_generator.py
+++ Text Generator/task/text_generator/text_generator.py
@@ -1,125 +1,72 @@
-# Write your code here
 from nltk.tokenize import WhitespaceTokenizer
-from nltk.util import bigrams
+from nltk.util import ngrams
 import nltk
 import random
 import re
-from collections import Counter
 
 nltk.download('punkt')
 
-filename = str(input())
+filename = str(input())  # Reading filename
 f = open(filename, "r", encoding="utf-8")  # Opening file with corpus
 
 corpus = f.read()  # Reading corpus from a file
 
-wt = WhitespaceTokenizer()
+wt = WhitespaceTokenizer()  # Creating tokenizer object
 tokens = wt.tokenize(corpus)  # Tokenizing corpus
 
 
-# _________________________SINGLE TOKEN STATISTICS OUTPUT_________________________
+# ____________________________TRIGRAMS CREATION AND USING____________________________
 
-# # Calculating and printing statistics
-# print('Corpus statistics')
-# print('All tokens:', len(tokens))
-# print('Unique tokens:', len(set(tokens)))
-# # print(sorted(set(tokens)))
-# print('')
-#
-# user_input = input()
-# while user_input != 'exit':
-#
-#     try:
-#         print(tokens[int(user_input)])
-#     except IndexError:
-#         print('Index Error. Please input an integer that is in the range of the corpus.')
-#     except TypeError:
-#         print('Type Error. Please input an integer.')
-#     except ValueError:
-#         print('Type Error. Please input an integer.')
-#     finally:
-#         user_input = input()
+trigrams = ngrams(tokens, 3)  # Creating trigrams collection
 
+# Creating Marcov chain model for trigrams
+markov_dict = {}  # Dictionary for frequency counting
+bigrams_collection = []  # List of bigrams made of (trigrams[0] + trigrams[1], trigrams[2])
+for trigram in trigrams:  # For every trigram
+    t_head = trigram[0] + ' ' + trigram[1]  # Creating head as a set of 2 first tokens
+    t_tail = trigram[2]  # And a tail as a third token
+    bigrams_collection.append((t_head, t_tail))  # Storing a set in the list
 
-# _________________________CREATING BIGRAMS_________________________
-
-bigrams_generator = bigrams(tokens)
-bigrams_collection = list(bigrams_generator)
-
-
-# _________________________BIGRAMS STATISTICS AND INFO OUTPUT_________________________
-
-# print('Number of bigrams:', len(bigrams_collection))
-# print('')
-# user_input = input()
-# while user_input != 'exit':
-#     try:
-#         print('Head:', list(bigrams_collection)[int(user_input)][0], 'Tail:', list(bigrams_collection)[int(user_input)][1])
-#     except IndexError:
-#         print('Index Error. Please input a value that is not greater than the number of all bigrams.')
-#     except TypeError:
-#         print('Typ Error. Please input an integer.')
-#     except ValueError:
-#         print('Typ Error. Please input an integer.')
-#     finally:
-#         user_input = input()
-
-
-# _________________________CREATING MARKOV CHAIN MODEL_________________________
-
-markov_dict = {}
-
-for bigram in bigrams_collection:
-    markov_dict.setdefault(bigram[0], {})
-    freq_dict = markov_dict[bigram[0]]
-    tail = bigram[1]
-    freq_dict.setdefault(tail, 0)
-    freq_dict[tail] += 1
-    markov_dict[bigram[0]] = freq_dict
-
-# user_input = input()
-# while user_input != 'exit':
-#     print('Head:', user_input)
-#     try:
-#         freq_dict = dict(markov_dict[user_input])
-#     except KeyError:
-#         print('Key Error. The requested word is not in the model. Please input another word.')
-#     else:
-#         freq_dict = dict(sorted(freq_dict.items(), key=lambda item: item[1], reverse=True))
-#         for obj in dict(freq_dict).items():
-#             print('Tail:', obj[0], 'Count:', obj[1])
-#     finally:
-#         print('')
-#         user_input = input()
+# Calculating appearance frequency for every third word (or second object of set)
+for bigram in bigrams_collection:  # For every bigram
+    markov_dict.setdefault(bigram[0], {})  # Setting empty dictionary as default
+    freq_dict = markov_dict[bigram[0]]  # Reading previous frequency data for current bigram
+    tail = bigram[1]  # Reading current tail
+    freq_dict.setdefault(tail, 0)  # Setting empty dictionary as default
+    freq_dict[tail] += 1  # Calculating appearance frequency for current tail
+    markov_dict[bigram[0]] = freq_dict  # Storing calculated frequency date
 
 
 # _________________________GENERATING RANDOM TEXT_________________________
 
-# print([token for token in tokens if re.match(r'[A-Z][a-z]+\Z', token) is not None])
-# # print([re.match(r'[A-Z][a-z]+', token).string for token in tokens])
-# try:
-#     print([re.match(r'[A-Z][a-z]+', token).string for token in tokens])
-# except AttributeError:
-#     pass
+start_list = []  # List for first token to start a sentence
+for i in range(10):  # Repeating sentence generation 10 times
+    for bigram in bigrams_collection:  # For every bigram
+        # If string of 2 word start with capital letter and don`t have sentence ending punctuation
+        if re.match(r'[A-Z][a-z ]+\Z', bigram[0]) is not None:
+            start_list.append(bigram[0])  # Append current string of 2 words to sentence start tokens
 
-for i in range(10):
-    first_word = random.choice([token for token in tokens if re.match(r'[A-Z][a-z]+\Z', token) is not None])
-    head = first_word
-    sentence = []
-    sentence += [first_word]
-    for x in range(3):
-        sentence += random.choices(list(dict(markov_dict[head]).keys()), list(dict(markov_dict[head]).values()))
-        head = sentence[len(sentence) - 1]
+    first_word = random.choice(start_list)  # Choosing start for sentence randomly
+    head = first_word  # Creating head as a last two words of sentence
+    sentence = []  # List for current sentence storing
+    sentence += [first_word]  # Append first two words to sentence
+    for x in range(2):  # Repeating 2 times for creating minimal sentence length of 5 words
+        keys = list(dict(markov_dict[head]).keys())
+        values = list(dict(markov_dict[head]).values())
+        sentence += random.choices(keys, values)  # Choosing next word of sentence randomly
+        # Creating head as a last two words of sentence
+        head = str('{} {}'.format(str(sentence[len(sentence) - 2]).split(' ')[-1], sentence[len(sentence) - 1]))
+
     exit_flag = False
-    while exit_flag is not True:
+    while exit_flag is not True:  # While token with sentence ending punctuation didn`t chose randomly
+        # Repeat next word choosing procedure
         sentence += random.choices(list(dict(markov_dict[head]).keys()), list(dict(markov_dict[head]).values()))
-        head = sentence[len(sentence) - 1]
+        head = str('{} {}'.format(str(sentence[len(sentence) - 2]).split(' ')[-1], sentence[len(sentence) - 1]))
+        # Checking for sentence ending punctuation
         if str(head).endswith('.'):
             exit_flag = True
         elif str(head).endswith('!'):
             exit_flag = True
         elif str(head).endswith('?'):
             exit_flag = True
-        # print(head)
-    print(*sentence)
-    # print('')
+    print(*sentence)  # Printing new sentence
